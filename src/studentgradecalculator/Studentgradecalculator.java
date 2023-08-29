f9fa5c2df1371a50712dd8072a53e361081140f5
Muhammad Muheeb Javed <muheebjavid@gmail.com>
2023-08-29 11:17:50 +0500

    Initial commit

---
+++ src/studentgradecalculator/Studentgradecalculator.java
@@ -0,0 +1,45 @@
+
+package studentgradecalculator;
+  import java.util.Scanner;
+
+public class Studentgradecalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int totalSubjects;
+        System.out.print("Enter the number of subjects: ");
+        totalSubjects = scanner.nextInt();
+
+        int[] marks = new int[totalSubjects];
+        for (int i = 0; i < totalSubjects; i++) {
+            System.out.print("Enter marks obtained in subject " + (i + 1) + ": ");
+            marks[i] = scanner.nextInt();
+        }
+
+        int totalMarks = 0;
+        for (int mark : marks) {
+            totalMarks += mark;
+        }
+
+        double averagePercentage = (double) totalMarks / (totalSubjects * 100) * 100;
+
+        String grade;
+        if (averagePercentage >= 90) {
+            grade = "A";
+        } else if (averagePercentage >= 80) {
+            grade = "B";
+        } else if (averagePercentage >= 70) {
+            grade = "C";
+        } else if (averagePercentage >= 60) {
+            grade = "D";
+        } else {
+            grade = "F";
+        }
+
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Grade: " + grade);
+    }
+}
+
